f5bde86c4b8da6938fca91976fa154310fd08e44
Ishan Pranav <ishan.pranav@outlook.com>
2024-10-21 21:45:10 -0400

    Add back

---
+++ src/main.c
@@ -0,0 +1,110 @@
+// main.c
+// Copyright (c) 2024 Ishan Pranav
+// Licensed under the MIT license.
+
+// References:
+//  - https://www.man7.org/linux/man-pages/man3/getopt.3.html
+//  - https://www.man7.org/linux/man-pages/man3/perror.3.html
+//  - https://www.man7.org/linux/man-pages/man3/sprintf.3p.html
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include "encoder.h"
+#include "euler.h"
+#include "mapped_file_collection.h"
+
+static void main_print_usage(String args[])
+{
+    printf("Usage: %s [OPTION]... FILE...\n", args[0]);
+}
+
+int main(int count, String args[])
+{
+    int option;
+    unsigned long long jobs;
+
+    while ((option = getopt(count, args, "hj:")) != -1)
+    {
+        switch (option)
+        {
+        case 'h':
+            main_print_usage(args);
+
+            return EXIT_SUCCESS;
+
+        case 'j':
+            jobs = strtoull(args[optind], NULL, 10);
+
+            if (!jobs)
+            {
+                main_print_usage(args);
+
+                return EXIT_FAILURE;
+            }
+            break;
+
+        default: return EXIT_FAILURE;
+        }
+    }
+
+    if (optind >= count)
+    {
+        main_print_usage(args);
+
+        return EXIT_FAILURE;
+    }
+
+    struct MappedFileCollection mappedFiles;
+    int fileCount = count - optind;
+    int ex = mapped_file_collection(&mappedFiles, args + optind, fileCount);
+
+    if (ex == -1)
+    {
+        perror(args[0]);
+
+        return EXIT_FAILURE;
+    }
+
+    if (ex < fileCount)
+    {
+        String path = args[optind + ex];
+        String errorMessage = malloc(strlen(args[0]) + strlen(path) + 3);
+
+        if (!errorMessage)
+        {
+            perror(args[0]);
+
+            return EXIT_FAILURE;
+        }
+
+        sprintf(errorMessage, "%s: %s", args[0], path);
+        perror(errorMessage);
+        free(errorMessage);
+
+        return EXIT_FAILURE;
+    }
+
+    if (!jobs)
+    {
+        Encoder encoder = { 0 };
+
+        for (int i = 0; i < fileCount; i++)
+        {
+            if (!encoder_next_encode(&encoder, mappedFiles.items + i))
+            {
+                perror(args[0]);
+                finalize_mapped_file_collection(&mappedFiles);
+
+                return EXIT_FAILURE;
+            }
+        }
+
+        encoder_end_encode(encoder);
+    }
+
+    finalize_mapped_file_collection(&mappedFiles);
+
+    return EXIT_SUCCESS;
+}
